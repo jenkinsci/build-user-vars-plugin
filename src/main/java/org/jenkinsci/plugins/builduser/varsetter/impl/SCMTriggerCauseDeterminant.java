a62a5eae3f4c59a722ee61134e696f405a520123
Ingo Rissmann <irimann@team.mobile.de>
2016-06-23 15:22:18 +0200

    Set GitHub push user as BUILD_USER_ID

--- src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/SCMTriggerCauseDeterminant.java
+++ src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/SCMTriggerCauseDeterminant.java
@@ -3,8 +3,10 @@ package org.jenkinsci.plugins.builduser.varsetter.impl;
 import hudson.triggers.SCMTrigger;
 import hudson.triggers.SCMTrigger.SCMTriggerCause;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.builduser.utils.UsernameUtils;
 import org.jenkinsci.plugins.builduser.varsetter.IUsernameSettable;
 
@@ -18,6 +20,18 @@ public class SCMTriggerCauseDeterminant implements IUsernameSettable<SCMTrigger.
         if (cause != null) {
 			UsernameUtils.setUsernameVars("SCMTrigger", variables);
 			
+			// sets pushedBy provided by GitHubPushCause as BUILD_USER_ID
+			try {
+				Field pushedByField = cause.getClass().getDeclaredField("pushedBy");
+				pushedByField.setAccessible(true);
+				String pushedBy = (String) pushedByField.get(cause);
+				if (StringUtils.isNotEmpty(pushedBy)) {
+					variables.put(BUILD_USER_ID, pushedBy);
+				}
+			} catch (ReflectiveOperationException exception) {
+				// do nothing 
+			}
+
 			return true;
 		} else {
 			return false;
