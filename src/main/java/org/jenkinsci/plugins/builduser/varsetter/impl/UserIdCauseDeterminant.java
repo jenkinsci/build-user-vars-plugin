4882dad7c4682a4ac6b0dd9ab3abf1301c5595b3
jusschwa <jusschwa@ibm.com>
2020-07-14 11:54:52 -0500

    Adds build user's groups to variables

--- src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
+++ src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
@@ -5,6 +5,7 @@ import hudson.model.Cause.UserIdCause;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.builduser.BuildUser;
 import org.jenkinsci.plugins.builduser.utils.UsernameUtils;
 import org.jenkinsci.plugins.builduser.varsetter.IUsernameSettable;
 
@@ -12,6 +13,9 @@ import hudson.security.ACL;
 import hudson.tasks.Mailer;
 import hudson.model.User;
 import hudson.model.UserProperty;
+import java.util.logging.Logger;
+import jenkins.model.Jenkins;
+import org.acegisecurity.GrantedAuthority;
 
 /**
  * This implementation is used to determine build username variables from <b>{@link UserIdCause}</b>.
@@ -19,6 +23,7 @@ import hudson.model.UserProperty;
  * <ul>
  *   <li>{@link IUsernameSettable#BUILD_USER_ID}</li>
  *   <li>{@link IUsernameSettable#BUILD_USER_VAR_NAME}</li>
+ *   <li>{@link IUsernameSettable#BUILD_USER_VAR_GROUPS}</li>
  *   <li>{@link IUsernameSettable#BUILD_USER_FIRST_NAME_VAR_NAME}</li>
  *   <li>{@link IUsernameSettable#BUILD_USER_LAST_NAME_VAR_NAME}</li>
  * </ul>
@@ -28,6 +33,8 @@ import hudson.model.UserProperty;
 public class UserIdCauseDeterminant implements IUsernameSettable<UserIdCause> {
 	
 	final Class<UserIdCause> causeClass = UserIdCause.class;
+	private static final Logger log = Logger.getLogger(BuildUser.class.getName());
+
 
 	/**
 	 * {@inheritDoc}
@@ -43,8 +50,21 @@ public class UserIdCauseDeterminant implements IUsernameSettable<UserIdCause> {
 			String trimmedUserId = StringUtils.trimToEmpty(cause.getUserId());
 			String userid = trimmedUserId.isEmpty() ? ACL.ANONYMOUS_USERNAME : trimmedUserId;
 			variables.put(BUILD_USER_ID, userid);
-			
-            		User user=User.get(userid);
+			String groupString = "";
+			try {
+				GrantedAuthority[] authorities = Jenkins.getInstance().getSecurityRealm().loadUserByUsername(userid).getAuthorities();
+				String[] groups = new String[authorities.length];
+				for(int i = 0; i < authorities.length; i++)
+					groups[i] = authorities[i].getAuthority();
+				groupString = String.join(",", groups);
+			} catch (Exception err) {
+				// Error
+				log.warning(String.format("Failed to get groups for user: %s error: %s ", userid, err.toString()));
+			}
+			variables.put(BUILD_USER_VAR_GROUPS, groupString);
+
+
+			User user=User.get(userid);
             		if(null != user) {
             		    UserProperty prop = user.getProperty(Mailer.UserProperty.class);
             		    if(null != prop) {
