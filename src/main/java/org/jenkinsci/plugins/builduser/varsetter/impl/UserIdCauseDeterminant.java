8c35f9d5c3dcb81a9b9c22fc64c4ba8cd828420e
Niklas Walter <walter.niklas@gmail.com>
2025-01-27 10:16:47 +0100

    JENKINS-73420: Retrieve groups from User class instead of security realm (#160)

--- src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
+++ src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
@@ -11,6 +11,7 @@ import org.jenkinsci.plugins.builduser.utils.BuildUserVariable;
 import org.jenkinsci.plugins.builduser.utils.UsernameUtils;
 import org.jenkinsci.plugins.builduser.varsetter.IUsernameSettable;
 import org.jenkinsci.plugins.saml.SamlSecurityRealm;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
@@ -26,14 +27,14 @@ import java.util.stream.Collectors;
  */
 public class UserIdCauseDeterminant implements IUsernameSettable<UserIdCause> {
 
-	private static final Logger log = Logger.getLogger(UserIdCauseDeterminant.class.getName());
+    private static final Logger log = Logger.getLogger(UserIdCauseDeterminant.class.getName());
 
-	/**
-	 * {@inheritDoc}
-	 * <p>
-	 * <b>{@link UserIdCause}</b> based implementation.
-	 */
-	public boolean setJenkinsUserBuildVars(UserIdCause cause, Map<String, String> variables) {
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>{@link UserIdCause}</b> based implementation.
+     */
+    public boolean setJenkinsUserBuildVars(UserIdCause cause, Map<String, String> variables) {
         if (cause == null) {
             return false;
         }
@@ -47,59 +48,60 @@ public class UserIdCauseDeterminant implements IUsernameSettable<UserIdCause> {
 
         variables.put(BuildUserVariable.ID, userid);
 
-		setUserGroups(originalUserId, variables);
-		setUserEmail(originalUserId, variables);
+        setUserGroups(originalUserId, variables);
+        setUserEmail(originalUserId, variables);
 
         return true;
     }
 
-	private String mapUserId(String userId) {
-		try {
-			SecurityRealm realm = Jenkins.get().getSecurityRealm();
-			if (realm instanceof SamlSecurityRealm samlSecurityRealm) {
-				String conversion = samlSecurityRealm.getUsernameCaseConversion();
+    private String mapUserId(String userId) {
+        try {
+            SecurityRealm realm = Jenkins.get().getSecurityRealm();
+            if (realm instanceof SamlSecurityRealm samlSecurityRealm) {
+                String conversion = samlSecurityRealm.getUsernameCaseConversion();
                 return switch (conversion) {
                     case "lowercase" -> userId.toLowerCase();
                     case "uppercase" -> userId.toUpperCase();
                     default -> userId;
                 };
-			}
-		} catch (NoClassDefFoundError e) {
-			log.fine("It seems the saml plugin is not installed, skipping saml user name mapping.");
-		}
-		return userId;
-	}
+            }
+        } catch (NoClassDefFoundError e) {
+            log.fine("It seems the saml plugin is not installed, skipping saml user name mapping.");
+        }
+        return userId;
+    }
 
-	private void setUserGroups(String userId, Map<String, String> variables) {
-		try {
-			SecurityRealm realm = Jenkins.get().getSecurityRealm();
-			Collection<? extends GrantedAuthority> authorities = realm.loadUserByUsername2(userId).getAuthorities();
+    private void setUserGroups(String userId, Map<String, String> variables) {
+        try {
+            SecurityRealm realm = Jenkins.get().getSecurityRealm();
+            Optional.ofNullable(User.getById(userId, false))
+                    .map(User::impersonate2)
+                    .map(authentication -> authentication.getAuthorities().stream()
+                                    .map(GrantedAuthority::getAuthority)
+                                    .filter(authority -> authority != null && !authority.isEmpty())
+                                    .collect(Collectors.joining(","))
+                    ).ifPresentOrElse(groups ->
+						variables.put(BuildUserVariable.GROUPS, groups)
+                    , () ->
+                        variables.put(BuildUserVariable.GROUPS, "")
+                    );
+        } catch (Exception err) {
+            log.warning(String.format("Failed to get groups for user: %s error: %s ", userId, err));
+        }
+    }
 
-			String groups = authorities.stream()
-					.map(GrantedAuthority::getAuthority)
-					.filter(authority -> authority != null && !authority.isEmpty())
-					.collect(Collectors.joining(","));
+    private void setUserEmail(String userId, Map<String, String> variables) {
+        Optional.ofNullable(User.getById(userId, false))
+                .map(user -> user.getProperty(UserProperty.class))
+                .map(UserProperty::getAddress)
+                .map(StringUtils::trimToEmpty)
+                .ifPresent(address -> variables.put(BuildUserVariable.EMAIL, address));
+    }
 
-			if (!groups.isEmpty()) {
-				variables.put(BuildUserVariable.GROUPS, groups);
-			}
-		} catch (Exception err) {
-			log.warning(String.format("Failed to get groups for user: %s error: %s ", userId, err));
-		}
-	}
-
-	private void setUserEmail(String userId, Map<String, String> variables) {
-		Optional.ofNullable(User.getById(userId, false))
-				.map(user -> user.getProperty(UserProperty.class))
-				.map(UserProperty::getAddress)
-				.map(StringUtils::trimToEmpty)
-				.ifPresent(address -> variables.put(BuildUserVariable.EMAIL, address));
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Class<UserIdCause> getUsedCauseClass() {
-		return UserIdCause.class;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public Class<UserIdCause> getUsedCauseClass() {
+        return UserIdCause.class;
+    }
 }
