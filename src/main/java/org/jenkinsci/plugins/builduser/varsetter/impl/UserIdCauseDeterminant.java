6c65f56013f339cb83840aa245b6f19cd1babaa6
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-08-04 15:49:06 +0200

    Remove usages of Commons Lang 2

--- src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
+++ src/main/java/org/jenkinsci/plugins/builduser/varsetter/impl/UserIdCauseDeterminant.java
@@ -6,7 +6,7 @@ import hudson.security.ACL;
 import hudson.security.SecurityRealm;
 import hudson.tasks.MailAddressResolver;
 import jenkins.model.Jenkins;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.builduser.utils.BuildUserVariable;
 import org.jenkinsci.plugins.builduser.utils.UsernameUtils;
 import org.jenkinsci.plugins.builduser.varsetter.IUsernameSettable;
